5fb1558014255ed06f9bf6e2317d3794804ba284
Chaithanya45 <y.chaithanya45@gmail.com>
2024-09-15 14:16:43 +0530

    added frontend with shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant="destructive">Test Btn</Button>
+    </>
+  );
+}
+
+export default App;
