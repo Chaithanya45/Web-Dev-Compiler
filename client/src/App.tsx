45b7284365ffbd4af49e015da85315262c71a7c3
Chaithanya45 <y.chaithanya45@gmail.com>
2024-09-16 00:49:14 +0530

    added pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+// import { Button } from "./components/ui/button";
+import Home from "./pages/Home";
+import Compiler from "./pages/Compiler";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-      <Button variant="destructive">Test Btn</Button>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compiler" element={<Compiler />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {/* <Button variant="destructive">Test Btn</Button> */}
     </>
   );
 }
---
+++ client/src/pages/Compiler.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function compiler() {
+  return <div>compiler</div>;
+}
