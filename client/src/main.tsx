0eb4a39d162df1af3b1c1df89f7690ca00884cda
Chaithanya45 <y.chaithanya45@gmail.com>
2024-09-21 22:40:07 +0530

    new change

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider>
+      <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
